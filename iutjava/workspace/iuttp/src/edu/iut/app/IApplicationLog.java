6c7c34fe6c2e9875dd73d02da357b485832086d3
ValentinGaultier <valentin.gaultier@u-psud.fr>
2015-11-09 10:44:11 +0100

    TP1

--- iutjava/workspace/iuttp/src/edu/iut/app/IApplicationLog.java
+++ iutjava/workspace/iuttp/src/edu/iut/app/IApplicationLog.java
@@ -1,10 +1,10 @@
 package edu.iut.app;
 
 public interface IApplicationLog {
-	/** TP1 : Créérr les méthodes :
-	 - setMessage
-	  - getMessage
-	  - addListener
-	  - getApplicationLogListeners();
-	*/
+	
+	void setMessage(String message);
+	String getMessage
+	void addListener(IApplicationLogListener listener)
+	IapplicationLogListener getApplicationLogListeners();
+
 }
